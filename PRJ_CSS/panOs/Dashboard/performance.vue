d7c2e71fe8f4b50816d328218d2bdbc8d5baf2e5
song <qhftkf1@naver.com>
2019-04-23 19:29:05 +0900

    dd

---
+++ PRJ_CSS/panOs/Dashboard/performance.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="line col-md-5 component1">
+    <div class="panel panel-default system">
+     <div class="panel-heading">System</div>
+     <div class="panel-body">
+       <p>Hostname:Interactor</p>
+       <p>IP:10.109.8.17</p>
+       <p>Netmask:255.255.255.0</p>
+       <p>Gateway:10.109.8.1</p>
+       <p>Version:3.1.5r2</p>
+       <p>Updated:Apr 10, 2019 4:52:10</p>
+       <p>License:Trial, Expires 9/1/2019</p>
+       <p>Uptime:105days</p>
+       <p>Since:Mar 31, 2019</p>
+       <p>Serial:Tr23450693</p>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  th{
+    padding-left:10px;
+    padding-right:20px;
+  }
+  .row{
+
+  }
+  .line{
+    border:1px solid grey;
+  }
+  .component1, .component2, .component3{
+    height:350px;
+  }
+  .system{
+    width:90%;
+    height:100%;
+  }
+  .panel-default{
+    margin-left:5%;
+    margin-right:5%;
+  }
+  .resources{
+    width:80%;
+    height:100%;
+  }
+  .storage{
+    width:70%;
+    height:100%;
+  }
+  .table{
+    margin-top:20%;
+    margin-bottom:10%;
+  }
+</style>
---
+++ PRJ_CSS/panOs/Dashboard/storage.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="line col-md-4 component3">
+    <div class="panel panel-info storage">
+     <div class="panel-heading">Storage</div>
+     <div class="panel-body">
+       <table align="center">
+         <tr style="border-bottom: 2px solid #ddd">
+           <th>Disk</th>
+           <th>Capacity</th>
+           <th>Status</th>
+         </tr>
+         <tr>
+           <th>Disk1</th>
+           <th>1Tb</th>
+           <th>Ok</th>
+         </tr>
+         <tr>
+           <th>Disk2</th>
+           <th>1Tb</th>
+           <th>Ok</th>
+         </tr>
+         <tr>
+           <th>Disk3</th>
+           <th>1Tb</th>
+           <th>Ok</th>
+         </tr>
+         <tr>
+           <th>Disk4</th>
+           <th>1Tb</th>
+           <th>Ok</th>
+         </tr>
+         <tr>
+           <th>Disk5</th>
+           <th>1Tb</th>
+           <th>Ok</th>
+         </tr>
+       </table>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+p{
+  font-size:12px;
+}
+th{
+  padding-left:10px;
+  padding-right:20px;
+}
+.row{
+
+}
+.line{
+  border:1px solid white;
+}
+.component1, .component2, .component3{
+  height:350px;
+}
+.component4{
+  height:260px;
+}
+.component5{
+  height:200px;
+}
+.system{
+  width:90%;
+  height:100%;
+}
+.panel-info{
+  margin-left:20px;
+  margin-right:5%;
+
+}
+.title{
+  backgroud-color:#d1dcfc;
+}
+.resources{
+  width:80%;
+  height:100%;
+}
+.storage{
+  width:70%;
+  height:100%;
+}
+.performance{
+  margin-top:30px;
+  width:91%;
+  height:90%;
+}
+.events{
+  margin-top:30px;
+  width:91%;
+  height:80%;
+}
+.table{
+  margin-top:20%;
+  margin-bottom:10%;
+}
+</style>
